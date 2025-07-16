5a252bf6b340da3b325f32df20961f28e60b4271
aurianebgnl <auriane.bgnl@gmail.com>
2025-07-16 11:27:11 +0200

    fix:fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,12 @@
 const fizzbuzz = (number) => {
-  if (number % 3 == 0) {
+  if (number % 3 == 0 && number != 5 && number != 15) {
     return "Fizz"
   }
   if (number === 5) {
     return "Buzz"
+  }
+  if (number === 15) {
+    return "Fizzbuzz"
   }
   return number
 }
